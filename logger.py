e660866c6315c561e461a655dbcfd4d19924bede
Martijn Bentum <bentummartijn@gmail.com>
2022-12-08 08:56:18 +0100

    logger object for faster logging; devs loads multiple mic arrays

--- logger.py
+++ logger.py
@@ -7,11 +7,12 @@ from tuning import Tuning
 import usb.core
 import usb.util
 
-dev = usb.core.find(idVendor=0x2886, idProduct=0x0018)
+devs = usb.core.find(find_all=True, idVendor=0x2886, idProduct=0x0018)
 
 class Report:
     ''' logs direction of sound angle and voice activity detection
-    cannot go faster than 1 hz, due to scheduler limitation
+    scheduler can be cpu intensive 
+    does not reliably go faster than 1 hz
     '''
     def __init__(self,name, interval= 1):
         print('starting',time.time())
@@ -19,13 +20,13 @@ class Report:
         self.filename = name + '.log'
         init_file(self.filename)
         self.interval = interval
-        self.tuning = Tuning(dev)
+        self.tunings = [Tuning(dev) for dev in devs]
         self.every = scheduler.every(interval, function = _report,
-            maximum_nexecuters = 1,args = (self.tuning,self.filename,))
+            maximum_nexecuters = 1,args = (self.tunings,self.filename,))
         self.running = True
              
     def stop(self):
-        try: self.tuning.close()
+        try: [tuning.close() for tuning in self.tunings]
         except AttributeError: print('could not close tuning connection')
         self.every.stop()
         self.running = False
@@ -42,39 +43,70 @@ def init_file(filename):
     with open(filename,'w') as fout:
         pass
         
-def _report(tuning, filename = None):
-    line = tuning.direction, tuning.is_voice(), time.time()
+def _report(tunings, filename = None):
+    line = []
+    for tuning in tunings:
+        line.extend([ tuning.direction, tuning.is_voice(),time.time() ] )
     if filename: log_report(line, filename)
     else: print(line)
 
 
 class Logger:
     ''' logs direction of sound angle and voice activity detection
-    can go faster than 1 hz, due to scheduler limitation
+    does not use scheduler less cpu load can go faster than 1 hz
     '''
     def __init__(self, name, interval = 0.2):
+        self.name = name
+        self.filename = name + '.log'
+        init_file(self.filename)
+        self.interval = interval
+        self.tunings = [Tuning(dev) for dev in devs]
+        self.every = scheduler.every(function = _logging,
+            maximum_nexecuters = 1,args = (self.tunings,self.filename,interval), 
+            n_times = 1)
+        self.running = True
 
+    def stop(self):
+        '''stop logging data to file and close usb connection to mic array.'''
+        stop_logging()
+        close_connection(self.tunings)
+        self.running = False
 
-def _logging(tuning,filename, interval = 0.2):
+    
+def _logging(tunings,filename, interval = 0.2):
+    '''fast logging of direction of sound and voice activity detection.'''
     start = time.time()
     while True:
         if time.time() - start > interval:
             start = time.time()
-            line = tuning.direction, tuning.is_voice(), time.time()
+            line = []
+            for tuning in tunings:
+                line.extend([tuning.direction, tuning.is_voice(),time.time()])
             if filename: log_report(line, filename)
             else: print(line)
-
-def start_logging(tuning,filename):
-    print('record to file:',filename)
-    p = multiprocessing.Process(
-        target = _logging,
-        args=(tuning,filename,),
-        )
-    p.start()
-    return p
+            if os.path.isfile('stop_logging'): break
 
 
-class controller:
-    def __init__(self, name, interval = .1):
-        self.name = name
-        self.interval = interval
+def stop_logging():
+    '''creates a flag to stop logging with the _logging function.
+    '''
+    with open('stop_logging','w') as fout:
+        pass
+    every = scheduler.every(interval = 2, function = _remove_stop_logging_flag, 
+        maximum_nexecuters = 1, n_times = 1)
+
+def _remove_stop_logging_flag():
+    '''remove the stop logging flag.'''
+    os.remove('stop_logging')
+
+
+def close_connection(tunings):
+    '''close the usb connection with the mic arrary after 2 seconds.'''
+    every = scheduler.every(interval = 2,function = _close_usb_connection,
+        maximum_nexecuters = 1,args = (tunings,), n_times = 1)
+
+def _close_usb_connection(tunings):
+    '''close the usb connection with the mic arrary.'''
+    try: [tuning.close() for tuning in tunings]
+    except AttributeError: print('could not close tuning connection')
+
