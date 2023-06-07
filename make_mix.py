0b99043596ad515bb1991b23de5d449e45718f96
Martijn Bentum <bentummartijn@gmail.com>
2023-06-07 16:45:12 +0200

    added functions to create all possible speaker sets for the different recordings

--- make_mix.py
+++ make_mix.py
@@ -1,8 +1,11 @@
 import glob
 import handle_phrases 
 import mixer
+import random
 
-mixed_audio = '/Users/u050158/mixed_audio/'
+random.seed(9)
+
+mixed_audio = '/Volumes/INTENSO/second_recording_session/'
 tone_audio = '/Users/u050158/tone_mixed_audio/'
 
 def make_all_mixes(tables = None):
@@ -31,6 +34,47 @@ def add_start_tone_to_audio_files(input_dir=mixed_audio,output_dir=tone_audio):
         output_filename = output_dir + 'tone_' + f.split('/')[-1]
         print(f, output_filename)
         mixer.add_start_tone(f, output_filename)
+    
+def get_all_table_ids():
+    with open('../table_ids') as fin:
+        t = fin.read().split('\n')
+    return t
+
+def make_recording_id_sets(n_recordings = 3):
+    table_ids = get_all_table_ids()
+    ids = table_ids[:]
+    output = []
+    while len(ids) > n_recordings:
+        recording_set = random.sample(ids,n_recordings)
+        ids = [x for x in ids if x not in recording_set]
+        output.append(recording_set)
+    if len(ids) > 0:
+        other_ids = [x for x in table_ids if x not in ids]
+        n = n_recordings - len(ids)
+        output.append(ids + random.sample(other_ids,n))
+    return output
+        
+def recording_sets_to_mix(tables, recording_sets):
+    for recording_set in recording_sets:
+        print('mixing set:', recording_set)
+        make(tables = tables, tids = recording_set)
+        
+def make_mixes(tables = None):
+    if not tables: tables = handle_phrases.Tables()
+    print('6 speakers')
+    recording_sets_6spk = make_recording_id_sets(3)
+    recording_sets_to_mix(tables, recording_sets_6spk)
+    print('4 speakers')
+    recording_sets_4spk = make_recording_id_sets(2)
+    recording_sets_to_mix(tables, recording_sets_4spk)
+    print('2 speakers')
+    recording_sets_2spk = make_recording_id_sets(1)
+    recording_sets_to_mix(tables, recording_sets_2spk)
+        
+        
+        
+        
+    
     
 
     
