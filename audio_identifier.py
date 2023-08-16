ed0402aedec62315ecac3aaa2dda05bc68547d1a
martijnbentum <bentummartijn@gmail.com>
2023-08-16 17:20:52 +0200

    added Audio_ids and Audio_id class to handle expected start and end times to find them in the long recordings

--- audio_identifier.py
+++ audio_identifier.py
@@ -49,3 +49,162 @@ def record(text, output_filename):
     os.system('rm ' + output_filename + '.aiff')
 
     
+
+
+audio_id_filename ='../mix_id_to_audio_id_mapping.txt'
+audio_id_original_filename='../audio_id_mapping_orginal_ifadv.txt'
+section_directory = '/Volumes/INTENSO/third_session/third_recording_session'
+
+def open_mix_file(filename):
+    with open(filename) as fin:
+        o = [line.split('\t') for line in fin.read().split('\n') if line]
+    return o
+
+class Audio_ids:
+    def __init__(self, audios = None): 
+        if not audios: 
+            import audio
+            audios = audio.make_audios()
+        self.audios = audios
+        self._audio_id = open_mix_file(audio_id_filename)
+        self._audio_id_original = open_mix_file(audio_id_original_filename)
+        self._set_info()
+        self.start_tone_duration = 6
+        self.end_tone_duration = 6
+        
+    def _set_info(self):
+        self._fix_audio_id_order()
+        self.audio_ids = []
+        section_set_index = 0
+        last_n_speakers = False
+        for index,line in enumerate(self._audio_id + self._audio_id_original):
+            aid = Audio_id(line, index, section_set_index, self)
+            if last_n_speakers and last_n_speakers != aid.n_speakers:
+                aid.section_set_index = 0
+                section_set_index = 1
+            else: section_set_index += 1
+            last_n_speakers = aid.n_speakers
+            self.audio_ids.append(aid)
+
+    def _fix_audio_id_order(self):
+        temp = []
+        for n in [6,4,2]:
+            for line in self._audio_id:
+                n_speakers = len(line[0].split('-'))
+                if n == n_speakers: temp.append(line)
+        self._audio_id = temp
+
+class Audio_id:
+    def __init__(self,line, recording_index, section_set_index, audio_ids):
+        self.line = line
+        self.recording_index = recording_index
+        self.section_set_index = section_set_index
+        self.audio_ids = audio_ids
+        self._set_info()
+        self._get_audios()
+        self._estimate_timestamps()
+
+    def __repr__(self):
+        m = self.section_name + ' ' + str(self.n_speakers)
+        m += ' ri ' + str(self.recording_index) 
+        m +=  ' si ' + str(self.section_set_index)
+        m += ' ' + self.section_set_name
+        m += ' start: ' + str(round(self.timestamps.start,2))
+        m += ' dur: ' + str(round(self.timestamps.duration,2))
+        return m
+
+    def _set_info(self):
+        self.section_name = self.line[0]
+        self.filename_audio_id = self.line[1]
+        self.audio_id_word = self.line[1].split('/')[-1].replace('.wav','')
+        self._set_original()
+        self._set_n_speakers()
+        n = 'spk-' + str(self.n_speakers)
+        if self.original: n += '_org'
+        else: n += '_mix'
+        self.section_set_name = n
+
+    def _set_original(self):
+        if '-' not in self.section_name: self.original = True
+        else: self.original = False
+
+    def _set_n_speakers(self):
+        if self.original: self.n_speakers = 2
+        else: self.n_speakers = len(self.section_name.split('-'))
+
+    def _get_section_audio(self):
+        if self.original: name = 'original'
+        else: name = 'section'
+        self.section_audio = []
+        for k,audio in self.audio_ids.audios[name].items():
+            if self.section_name in k: self.section_audio.append(audio)
+
+    def _get_tone_audio(self):
+        if self.original: name = 'original_tone'
+        else: name = 'tone'
+        self.tone_audio = []
+        for k,audio in self.audio_ids.audios[name].items():
+            if self.section_name in k: self.tone_audio.append(audio)
+
+    def _get_combined_audio(self):
+        if self.original: name = 'original_combined'
+        else: name = 'combined'
+        self.combined_audio = []
+        identifier = 'nch-' + str(self.n_speakers)
+        for k,audio in self.audio_ids.audios[name].items():
+            if identifier in k: self.combined_audio.append(audio)
+
+    def _get_audios(self):
+        self._get_section_audio()
+        self._get_tone_audio()
+        self._get_combined_audio()
+
+    def _estimate_timestamps(self):
+        word = self.audio_id_word
+        temp = self.audio_ids.audios['audio_id'][word]
+        self.audio_id_word_duration = temp.seconds
+        self.timestamps = Timestamps(self)
+        
+class Timestamps:
+    def __init__(self,audio_id):
+        self.audio_id = audio_id
+        self.recording_index = self.audio_id.recording_index
+        self._set_previous()
+        self._estimate_tone()
+        self._estimate_section()
+
+    def _set_previous(self):
+        if self.recording_index == 0: self.previous_audio_id = None
+        else: 
+            previous_index = self.recording_index - 1
+            audio_ids = self.audio_id.audio_ids.audio_ids
+            self.previous_audio_id = audio_ids[previous_index]
+
+    def _estimate_tone(self):
+        if self.recording_index == 0: 
+            self.start_tone = 0
+        else: 
+            self.start_tone = self.previous_audio_id.timestamps.end_tone
+        self.end_tone = self.start_tone + self.audio_id.tone_audio[0].seconds
+        self.duration_tone = self.end_tone - self.start_tone
+
+    def _estimate_section(self):
+        self.preamble = 6
+        self.preamble += self.audio_id.audio_id_word_duration
+        self.start = self.start_tone + self.preamble
+        self.end = self.start + self.audio_id.section_audio[0].seconds
+        self.duration = self.end - self.start
+        
+    def estimate_sample_index_final_start_tone(self, sample_rate):
+        return int(round((self.start - 2) * sample_rate,0))
+
+    def estimate_sample_index_first_end_tone(self, sample_rate):
+        return int(round((self.end + 1) * sample_rate,0))
+       
+        
+
+def _find_transcription_filename_section_mix(section_name):
+    d = section_directory
+    fn = glob.glob(d + '*'+ section_name + '*.txt')
+    if fn: return fn[0]
+    print('could not find section_name')
