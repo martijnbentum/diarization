5897fdedfc10182e50824d27b2fb52ea09f6ea05
martijnbentum <bentummartijn@gmail.com>
2023-09-08 09:06:54 +0200

    added delta option to create textgrids with offset

--- table_to_textgrid.py
+++ table_to_textgrid.py
@@ -5,37 +5,37 @@ from praatio import textgrid
 from praatio.utilities.constants import Interval
 from praatio.data_classes import interval_tier
 
-def all_text_files_to_textgrid():
+def all_text_files_to_textgrid(delta = 2):
     directory = locations.play_transcription_tables_directory
     fn = glob.glob(directory + '*.txt')
     textgrids = []
     for f in fn:
-        filename_to_textgrid(f)
+        filename_to_textgrid(f, delta)
     return textgrids
 
-def filename_to_textgrid(filename):
+def filename_to_textgrid(filename, delta = 0):
     temp = filename.split('/')[-1].replace('.txt','.textgrid')
     output_filename = locations.play_textgrids_directory
     output_filename += temp
     text = load_text(filename)
-    textgrid = text_to_textgrid(text)
+    textgrid = text_to_textgrid(text, delta)
     textgrid.save(output_filename, 'short_textgrid', True)
     return textgrid
 
-def text_to_textgrid(text):
+def text_to_textgrid(text, delta = 0):
     tg = textgrid.Textgrid()
-    interval_dict = make_speaker_to_interval_dict(text)
+    interval_dict = make_speaker_to_interval_dict(text, delta)
     for speaker, intervals in interval_dict.items():
         tier = interval_tier.IntervalTier(name = speaker, entries = intervals)
         tg.addTier(tier)
     return tg
 
-def make_speaker_to_interval_dict(text):
+def make_speaker_to_interval_dict(text, delta = 0):
     d = {}
     for line in text:
         speaker = line[0]
         if speaker not in d.keys(): d[speaker] = []
-        interval = line_to_interval(line)
+        interval = line_to_interval(line, delta)
         d[speaker].append(interval)
     return d
 
@@ -53,8 +53,8 @@ def phrase_to_interval(phrase):
 def turn_to_interval(turn):
     return phrase_to_interval(turn)
 
-def line_to_interval(line):
-    start = float(line[1])
+def line_to_interval(line, delta = 0):
+    start = float(line[1]) + delta
     label = line[2]
-    end = float(line[3])
+    end = float(line[3]) + delta
     return Interval(start,end,label)
