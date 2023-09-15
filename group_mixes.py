09f01b12af02feb93e0a748a7de9031f7053482f
martijnbentum <bentummartijn@gmail.com>
2023-09-15 11:36:21 +0200

    add comments

--- group_mixes.py
+++ group_mixes.py
@@ -1,8 +1,15 @@
+'''group the channels for different sections together in dictionary
+'''
+
 import glob
 import locations
 
 
 def group_n_speakers(n_speakers = 6, directory =None):
+    '''map section id to channel wav filename
+    n_speakers      the number of speakers
+    directory       the directory to search for audio files
+    '''
     if directory == None: directory = locations.section_directory
     fn = glob.glob(directory + 'nch-'+str(n_speakers)+'*ch*_spk*.wav')
     d = {}
@@ -13,12 +20,14 @@ def group_n_speakers(n_speakers = 6, directory =None):
     return d
 
 def group_all_files(directory =None):
+    '''group all audio files'''
     d = {}
     for n_speakers in [2,4,6]:
         d.update(group_n_speakers(n_speakers))
     return d
 
 def group_original_ifadv_channels():
+    '''group audio for each channel (2) of the original ifadv audio files.'''
     fn = glob.glob(locations.original_directory + '*.wav')
     d = {}
     for f in fn:
